e251e6ba42c50eafa71f208fad439ee7c1bef352
Bhupendra Madhu <bhupendra@nimbleappgenie.com>
2025-01-09 17:32:25 +0530

    User Authentication setupnpm i @types/ms

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,37 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './users/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-	imports: [ConfigModule.forRoot()],
+	imports: [
+		ConfigModule.forRoot(),
+		JwtModule.register({
+			global: true,
+			secret: process.env.JWT_SECRET,
+		}),
+		TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: process.env.DB_HOST,
+			database: process.env.DB_NAME,
+			username: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			logger: 'file',
+			logging: 'all',
+			entities: [User],
+			synchronize: true,
+		}),
+		AuthModule,
+		UsersModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+	constructor(private dataSource: DataSource) {}
+}
