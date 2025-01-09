1a1a6eb9f5e8d1922f639b79baa3927c01644d30
Bhupendra Madhu <bhupendra@nimbleappgenie.com>
2025-01-09 13:24:19 +0530

    Base controller ready

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,27 @@ export class AppController {
 	constructor(private readonly appService: AppService) {}
 
 	@Get()
-	getHello(): string {
-		return this.appService.getHello();
+	async getHello(): Promise<object> {
+		return await this.appService.getHello();
+	}
+
+	@Post()
+	async postHello(): Promise<object> {
+		return await this.appService.postHello();
+	}
+
+	@Put()
+	async putHello(): Promise<object> {
+		return await this.appService.putHello();
+	}
+
+	@Patch()
+	async patchHello(): Promise<object> {
+		return await this.appService.patchHello();
+	}
+
+	@Delete()
+	async deleteHello(): Promise<object> {
+		return await this.appService.deleteHello();
 	}
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,38 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-	getHello(): string {
-		return 'Hello World!';
+	async getHello() {
+		return {
+			status: true,
+			message: 'This is a get route!',
+		};
+	}
+
+	async postHello() {
+		return {
+			status: true,
+			message: 'This is a post route!',
+		};
+	}
+
+	async putHello() {
+		return {
+			status: true,
+			message: 'This is a put route!',
+		};
+	}
+
+	async patchHello() {
+		return {
+			status: true,
+			message: 'This is a patch route!',
+		};
+	}
+
+	async deleteHello() {
+		return {
+			status: true,
+			message: 'This is a delete route!',
+		};
 	}
 }
